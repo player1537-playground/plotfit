a3d0d71d411d269ee2f91890daa4196e9f5489ff
Tanner Hobson <thobson125@gmail.com>
2016-06-13 11:56:55 -0400

    Fix fitting UI

--- site1/app1/static/app1/plot_fit.js
+++ site1/app1/static/app1/plot_fit.js
@@ -55,6 +55,7 @@ plotfit = (function(my, d3, math) {
       var newScope = {};
       variables.forEach(function(name) {
         if (scope.hasOwnProperty(name)) {
+          console.log("Found", name, "=", scope[name]);
           newScope[name] = scope[name];
         } else {
           newScope[name] = defaultValue;
@@ -67,6 +68,7 @@ plotfit = (function(my, d3, math) {
       compiled = parsed.compile();
 
       dispatch.expr.call(null, my);
+      dispatch.scope.call(null, my);
       dispatch.change.call(null, my);
 
       return my;
@@ -76,6 +78,19 @@ plotfit = (function(my, d3, math) {
       if (!arguments.length) return defaultValue;
       defaultValue = _;
       return my;
+    };
+
+    my.copy = function() {
+      var scopeCopy = {};
+      d3.entries(my.scope()).forEach(function(d) {
+        scopeCopy[d.key] = d.value;
+      });
+
+      return expression()
+        .defaultValue(defaultValue)
+        .parameters(parameters)
+        .expr(expr)
+        .scope(scopeCopy);
     };
 
     my = d3.rebind(my, dispatch, 'on');
@@ -184,10 +199,12 @@ plotfit = (function(my, d3) {
             .attr('type', 'text')
             .attr('class', 'form-control pull-left');
           input
-            .property('value', d => d.textValue)
             .on('input', function(d) {
               dispatch.textChanged.apply(this, arguments);
             });
+          input.filter(function(d) {
+            return d3.select(this).property('value') !== d.textValue;
+          }).property('value', d => d.textValue);
 
           var vars = self.selectAll('.variables').data(d => [d.inputs]);
           vars.enter().append('div')
@@ -219,7 +236,7 @@ plotfit = (function(my, d3) {
             .attr('name', d => d.name);
           inputs.exit().remove();
           inputs
-            .attr('value', d => d.value)
+            .property('value', d => d.value)
             .on('input', function(d) {
               dispatch.inputChanged.apply(this, arguments);
             });
@@ -273,7 +290,6 @@ plotfit = (function(my, d3) {
 
           sidebarInput
             .on('dropdownSelected', function(d) {
-              console.log(d, this);
               scale.expr(d);
 
             }).on('buttonPressed', function(d) {
@@ -282,7 +298,7 @@ plotfit = (function(my, d3) {
 
             }).on('inputChanged', function(d) {
               var scope = scale.scope();
-              scope[d.name] = this.value;
+              scope[d.name] = +this.value;
               scale.scope(scope);
 
             }).on('textChanged', function(d) {
@@ -315,53 +331,162 @@ plotfit = (function(my, d3) {
 })(typeof plotfit==="undefined" ? {} : plotfit, Plotly.d3);
 
 plotfit = (function(my, d3) {
-  var fittings = d3.map();
-
   my.fitting = function fitting(dispatch) {
     dispatch = dispatch || d3.dispatch.apply(null, [
-      'parameters', 'scope', 'expr', 'change', 'doFit',
+      'parameters', 'scope', 'expr', 'change', 'active', 'data', 'domain',
     ]);
 
     var expression = plotfit.expression(dispatch)
           .defaultValue(1)
-          .parameters(['X']);
+          .parameters(['X']),
+        active = false,
+        data = null,
+        domain = [0, Infinity];
 
-    function my(data) {
-      var startingScope = expression.scope(),
+    function refit() {
+      if (data === null || !active) {
+        return;
+      }
+
+      var start = domain[0],
+          end = domain[1],
+          currentData = data.filter((d, i) => start <= i && i < end),
+          expressionCopy = expression.copy(),
+          startingScope = expressionCopy.scope(),
           paramNames = d3.keys(startingScope),
           startingValues = paramNames.map(d => startingScope[d]),
           results = null,
           newScope = null;
 
-      data = data.filter(d => isFinite(d[0]) && isFinite(d[1]));
+      currentData = currentData.filter(d => isFinite(d[0]) && isFinite(d[1]));
 
       function evaluate(x, params) {
         var scope = {};
         for (var i=0; i<params.length; ++i) {
           scope[paramNames[i]] = params[i];
         }
-        expression.scope(scope);
+        expressionCopy.scope(scope);
 
-        return expression.call(null, x);
+        return expressionCopy.call(null, x);
       };
 
-      results = cobyla.nlFit(data, evaluate, startingValues);
+
+      results = cobyla.nlFit(currentData, evaluate, startingValues, undefined, undefined, undefined, { maxFun: 100 });
 
       newScope = {};
       d3.zip(paramNames, results.params).forEach(function(d) {
         newScope[d[0]] = d[1];
       });
       expression.scope(newScope);
+    };
 
-      function fitted(x) {
-        return expression.call(null, x);
-      };
-
-      return fitted;
+    function my(x) {
+      return expression.call(null, x);
     }
+
+    my.active = function(_) {
+      if (!arguments.length) return active;
+      active = _;
+      refit();
+      dispatch.active.call(null, my);
+      dispatch.change.call(null, my);
+      return my;
+    };
+
+    my.data = function(_) {
+      if (!arguments.length) return data;
+      data = _;
+      refit();
+      dispatch.data.call(null, my);
+      dispatch.change.call(null, my);
+      return my;
+    };
+
+    my.domain = function(_) {
+      if (!arguments.length) return domain;
+      console.log("new domain", _);
+      domain = _;
+      refit();
+      dispatch.domain.call(null, my);
+      dispatch.change.call(null, my);
+      return my;
+    };
 
     my = d3.rebind(my, dispatch, 'on');
     my = d3.rebind(my, expression, 'scope', 'expr');
+
+    return my;
+  };
+
+  return my;
+
+})(typeof plotfit==="undefined" ? {} : plotfit, Plotly.d3);
+
+plotfit = (function(my, d3) {
+  my.fittingVC = function fittingVC() {
+    var fitting = plotfit.fitting(),
+        sidebarInput = plotfit.sidebarInput();
+
+    function my(selection) {
+      selection.each(function(data) {
+        var self = d3.select(this);
+
+        function redraw() {
+          var newData = {
+            dropdownLabel: 'Y \u2248 ',
+            options: data.options,
+            buttonActive: fitting.active(),
+            buttonLabel: 'fit',
+            textValue: fitting.expr(),
+            inputs: d3.entries(fitting.scope()).map(d => {
+              return {
+                name: d.key,
+                prettyName: d.key,
+                value: d.value === 0 ? '' : d.value,
+                placeholder: '0',
+              };
+            }),
+          };
+
+          var wrapper = self.selectAll(".wrapper").data([newData]);
+          wrapper.enter().append('div')
+            .attr('class', 'wrapper');
+          wrapper
+            .call(sidebarInput);
+
+          sidebarInput
+            .on('dropdownSelected.fittingVC', function(d) {
+              fitting.expr(d);
+
+            }).on('buttonPressed.fittingVC', function(d) {
+              fitting.active(!fitting.active());
+
+            }).on('inputChanged.fittingVC', function(d) {
+              var scope = fitting.scope();
+              scope[d.name] = +this.value;
+              fitting.scope(scope);
+
+            }).on('textChanged.fittingVC', function(d) {
+              fitting.expr(d3.select(this).property('value'));
+
+            });
+        }
+
+        redraw();
+
+        fitting.on('change.fittingVC', function() {
+          redraw();
+        });
+
+      });
+    }
+
+    my.fitting = function(_) {
+      if (!arguments.length) return fitting;
+      fitting.on('change.fittingVC', null);
+      fitting = _;
+      return my;
+    };
 
     return my;
   };
@@ -386,7 +511,9 @@ plotfit = (function(my, Plotly, d3) {
         title = "PlotFit Chart",
         heightPercent = 80,
         fittedFunction = null,
-        fittedLegendName = "Fit";
+        fittedLegendName = "Fit",
+        fitting = null,
+        fitDomain = [0, Infinity];
 
     function my(selection) {
       selection.each(function(fullData) {
@@ -396,7 +523,9 @@ plotfit = (function(my, Plotly, d3) {
             tangled = d3.zip(fullX, fullY),
             plottedX = tangled.map(d => xScale.apply(xScale, d)),
             plottedY = tangled.map(d => yScale.apply(yScale, d)),
+            plottedTangled = d3.zip(plottedX, plottedY),
             fittedY = null,
+            fittedX = null,
             trace = null,
             data;
 
@@ -418,13 +547,13 @@ plotfit = (function(my, Plotly, d3) {
           },
         };
 
-        if (fittedFunction !== null) {
-          var fittedDomain = fittedFunction.domain(),
+        if (fitting !== null && fitting.active()) {
+          var fittedDomain = fitting.domain(),
               start = fittedDomain[0],
               end = fittedDomain[1],
-              tangledFit = tangled.filter((d, i) => start <= i && i < end);
+              tangledFit = plottedTangled.filter((d, i) => start <= i && i < end);
           fittedY = tangledFit.map(function(d) {
-            return yScale(d[0], fittedFunction(d[0]));
+            return fitting(d[0]);
           });
           fittedX = tangledFit.map(d => d[0]);
 
@@ -549,9 +678,9 @@ plotfit = (function(my, Plotly, d3) {
       return my;
     };
 
-    my.fittedFunction = function(_) {
-      if (!arguments.length) return fittedFunction;
-      fittedFunction = _;
+    my.fitting = function(_) {
+      if (!arguments.length) return fitting;
+      fitting = _;
       return my;
     };
 
@@ -564,6 +693,12 @@ plotfit = (function(my, Plotly, d3) {
     my.colors = function(_) {
       if (!arguments.length) return colors;
       colors = _;
+      return my;
+    };
+
+    my.fitDomain = function(_) {
+      if (!arguments.length) return fitDomain;
+      fitDomain = _;
       return my;
     };
 
@@ -585,7 +720,9 @@ plotfit = (function(my, Plotly, d3) {
           xScaleVC = plotfit.scaleVC().scale(xScale),
           yScale = plotfit.scale().expr('I'),
           yScaleVC = plotfit.scaleVC().scale(yScale),
-          fitting = plotfit.fitting(),
+          fitting = plotfit.fitting().expr('m*X+b')
+            .data(fullData.map(d => [xScale(d.Q, d.I), yScale(d.Q, d.I)])),
+          fittingVC = plotfit.fittingVC().fitting(fitting),
           chart = plotfit.chart()
             .x(d => d.Q)
             .y(d => d.I)
@@ -593,8 +730,8 @@ plotfit = (function(my, Plotly, d3) {
             .heightPercent(80)
             .colors(d => "blue")
             .xScale(xScale)
-            .yScale(yScale),
-          fittedFunction = null;
+            .yScale(yScale)
+            .fitting(fitting);
 
       window.__fullData = fullData;
 
@@ -609,11 +746,21 @@ plotfit = (function(my, Plotly, d3) {
         dropdownLabel: 'Y = ',
       }).call(yScaleVC);
 
+      d3.select("#fitting-settings").datum({
+        options: ['m*X+b', 'a*X^2+b*X+c'],
+      }).call(fittingVC);
+
       xScale.on('change.main', function() {
         redraw();
+        fitting.data(fullData.map(d => [xScale(d.Q, d.I), yScale(d.Q, d.I)]));
       });
 
       yScale.on('change.main', function() {
+        redraw();
+        fitting.data(fullData.map(d => [xScale(d.Q, d.I), yScale(d.Q, d.I)]));
+      });
+
+      fitting.on('change.main', function() {
         redraw();
       });
 
@@ -674,10 +821,6 @@ plotfit = (function(my, Plotly, d3) {
       });
 
       function redraw() {
-        chart
-          .fittedFunction(fittedFunction)
-          .fittedLegendName(fitting.fittingName());
-
         plotContainer.data([fullData])
           .call(chart);
       }
@@ -696,56 +839,7 @@ plotfit = (function(my, Plotly, d3) {
         selectDataOldStart = start;
         selectDataOldEnd = end;
 
-        fittedFunction = fitting(fullData, start, end);
-        chart.fittedFunction(fittedFunction);
-
-        var params = fittedFunction.params;
-
-        console.log(params);
-
-        var parent = d3.select("#fitting-parameters");
-
-        var groups = parent.selectAll('.form-group').data(params);
-        groups.enter().append('div')
-          .attr('class', 'form-group')
-          .style('margin-bottom', '0px');
-        groups.exit().remove();
-
-        var labels = groups.selectAll('label').data(d => [d]);
-        labels.enter().append('label')
-          .attr("class", "col-sm-6 col-xs-6 control-label text-right");
-        labels.exit().remove();
-        labels
-          .text(d => d.prettyName);
-
-        var wrappers = groups.selectAll('div').data(d => [d]);
-        wrappers.enter().append('div')
-          .attr('class', 'col-sm-6 col-xs-6');
-        wrappers.exit().remove();
-
-        var inputs = wrappers.selectAll('input').data(d => [d]);
-        inputs.enter().append('input')
-          .attr('class', 'form-control')
-          .attr('type', 'text')
-          .attr('placeholder', '0')
-          .attr('name', d => d.name);
-        inputs.exit().remove();
-        inputs
-          .attr('value', d => d.value.toString().substring(0, 5))
-          .on('input', function(d) {
-            yScale.updateVariable(d.key, this.value);
-            redraw();
-          });
-
-        chart.colors(function(d, i) {
-          return (start <= i && i < end) ? "blue" : "steelblue";
-        });
-
-        fittedFunction.params.map(function(d) {
-          console.log(d.prettyName, "=", d.value);
-        });
-
-        redraw();
+        fitting.domain([start, end]);
       }
 
     });
