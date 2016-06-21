ca08814e1eb7eaa00f94ffc913361539207f7a59
Tanner Hobson <thobson125@gmail.com>
2016-06-21 14:50:01 -0400

    Attempt to modularize x-scale and sidebar-input

---
+++ assets/js/components/sidebar-input.vue
@@ -0,0 +1,56 @@
+<style>
+
+</style>
+
+<template>
+  <div class="input-group">
+    <div class="input-group-btn">
+      <button type="button" class="btn btn-default dropdown-toggle"
+              data-toggle="dropdown">
+        {{ dropdownLabel }}<span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu">
+        <li v-for="option in dropdownOptions">
+          <a href="#!" value="option" @click="inputTextInternal = option">{{ option }}</a>
+        <li>
+      </ul>
+      <label class="btn btn-default">
+        <input type="checkbox" v-model="buttonStateInternal">{{ buttonLabel }}
+      </label>
+    </div>
+    <input type="text" class="form-control pull-left"
+           v-model="inputTextInternal" debounce="500"/>
+  </div>
+</template>
+
+<script>
+
+  export default {
+      props: {
+          dropdownLabel: String,
+          dropdownOptions: Array,
+          buttonLabel: String,
+          inputText: {
+              type: String,
+          },
+          buttonState: {
+              type: Boolean,
+          },
+      },
+      data: function() {
+          return {
+          };
+      },
+      computed: {
+          inputTextInternal: {
+              get() { return this.inputText; },
+              set(val) { this.$dispatch('input-text', val); },
+          },
+          buttonStateInternal: {
+              get() { return this.buttonState; },
+              set(val) { this.$dispatch('button-state', val); },
+          },
+      },
+  }
+
+</script>
--- assets/js/components/x-scale.vue
+++ assets/js/components/x-scale.vue
@@ -3,29 +3,24 @@
 </style>
 
 <template>
-  <div class="input-group">
-    <div class="input-group-btn">
-      <button type="button" class="btn btn-default dropdown-toggle"
-              data-toggle="dropdown">
-        {{ dropdownLabel }}<span class="caret"></span>
-      </button>
-      <ul class="dropdown-menu">
-        <li v-for="option in standardValues">
-          <a href="#!" value="option" @click="updateExpr(option)">{{ option }}</a>
-        <li>
-      </ul>
-      <button type="button" class="btn btn-default"
-              :class="{ 'active': buttonActive }"
-              @click.prevent="buttonPressed">
-        {{ buttonLabel }}
-      </button>
-    </div>
-    <input type="text" class="form-control pull-left"
-           v-model="inputText" debounce="500"/>
+  <div>
+    <sidebar-input
+       dropdown-label="X = "
+       :dropdown-options="['Q', 'log(Q)', 'Q^2']"
+       button-label="log"
+       :input-text="inputText"
+       @input-text="updateInputText"
+       :button-state="buttonActive"
+       @button-state="updateButtonState"
+       >
+    </sidebar-input>
+
   </div>
 </template>
 
 <script>
+
+  import sidebarInput from './sidebar-input.vue';
 
   import { xScaleExpr, xScaleLogOrLinear } from '../vuex/getters';
   import { xScaleSetExpr, xScaleSetLogOrLinear } from '../vuex/actions';
@@ -33,13 +28,6 @@
   export default {
       data: function() {
           return {
-              standardValues: [
-                  'Q',
-                  'log(Q)',
-                  'Q^2',
-              ],
-              buttonLabel: 'log',
-              dropdownLabel: 'X = ',
           };
       },
       vuex: {
@@ -71,9 +59,15 @@
           },
       },
       methods: {
-          buttonPressed() {
-              this.buttonActive = !this.buttonActive;
+          updateInputText(val) {
+              this.inputText = val;
           },
+          updateButtonState(val) {
+              this.buttonActive = val;
+          },
+      },
+      components: {
+          sidebarInput,
       },
   }
 
